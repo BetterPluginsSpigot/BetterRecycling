ed8255823acdbeef6592e28eadf6fc7475039704
Michiel Proost <Michiel.Proost@student.uantwerpen.be>
2021-05-27 14:01:40 +0200

    Setup of repository.

---
+++ src/main/java/io/github/michielproost/betterrecycling/BetterRecycling.java
@@ -0,0 +1,22 @@
+package io.github.michielproost.betterrecycling;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * BetterRecycling Plugin:
+ * A plugin in which you can recycle materials into their crafting components.
+ * Author: Michiel Proost
+ */
+public class BetterRecycling extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // TODO. Register commands, events & runnables.
+    }
+
+    @Override
+    public void onDisable() {
+        // TODO. Unregister commands, events & runnables.
+    }
+
+}
